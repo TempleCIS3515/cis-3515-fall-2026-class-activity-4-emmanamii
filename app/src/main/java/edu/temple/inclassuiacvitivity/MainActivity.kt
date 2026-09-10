5a9aac03614218f70e1b30f82ff3aed1236b8a2c
EmmaMcGrath24. <emmamia2005@gmail.com>
2026-09-10 15:35:11 -0400

    Uploading files

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -11,16 +11,28 @@ class MainActivity : AppCompatActivity() {
 
         val spinner = findViewById<Spinner>(R.id.spinner)
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
-
-        /* TODO Step 2: Populate this array with multiples of 5 from 5 - 100*/
+        
         //val numberArray = Array Of Numbers
+        val numberArray = Array(20){(it + 1)* 5}
 
-        /* TODO Step 3: Create adapter to display items from array in Spinner */
         //spinner.adapter = ArrayAdapter...
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, numberArray.toList())
+        spinner.adapter = adapter
 
-
-        // TODO Step 4: Change TextView's text size to the number selected in the Spinner */
         //spinner.onItemSelectedListener = object: ...
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ){
+                displayTextView.textSize = parent?.getItemAtPosition(position).toString().toFloat()
+            }
 
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                //nothing to do here
+            }
+        }
     }
 }
